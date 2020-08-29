e0f8957c3fe618dc8cf97833f7a6cece5e7fe780
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-29 10:29:13 +0000

    Deploy Erriez/ErriezINA219 to github.com/Erriez/ErriezINA219.git:gh-pages

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['erriez_20ina219_20i2c_20voltage_2fcurrent_2fpower_20monitor_20library_20for_20arduino',['Erriez INA219 I2C Voltage/Current/Power Monitor library for Arduino',['../index.html',1,'']]]
+  ['erriez_20ina219_20i2c_2fsmb_20voltage_2fcurrent_2fpower_20monitor_20library_20for_20arduino',['Erriez INA219 I2C/SMB Voltage/Current/Power Monitor library for Arduino',['../index.html',1,'']]]
 ];
